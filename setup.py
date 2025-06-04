1a3639f9b04b0715e4f4f54eefd690af7a6732bf
Kalyanbrata Maity <maityk007@gmail.com>
2025-06-04 20:45:53 +0530

    added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text-summarizer"
+AUTHOR_USER_NAME = "KalyanbrataMaity"
+SRC_REPO = "text-summarizer-nlp-e2e"
+AUTHOR_EMAIL = "maityk007@gmail.com"
+
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Text Summarization using NLP End-to-End approach",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
+
